1ae6a1a49c2ad5c11bc9677148d3794cd04c99f6
Park-Jong-Min <jongmin15@postech.ac.kr>
2020-12-30 13:50:43 +0900

    Make attention image extract function for src attn

--- egs2/jm_test/asr/extract_attention_image.py
+++ egs2/jm_test/asr/extract_attention_image.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from espnet_model_zoo.downloader import ModelDownloader
 from espnet2.bin.asr_inference import Speech2Text
 
-def save_unmasked_image(image_list, name, n_layers, n_heads, PATH):
+def save_encoder_image(image_list, name, n_layers, n_heads, PATH):
     fig_saved_dir = PATH
 
     for layer in range(n_layers):
@@ -20,23 +20,27 @@ def save_unmasked_image(image_list, name, n_layers, n_heads, PATH):
         plt.savefig(fig_saved_dir + 'audio' + str(audio_num) + '_' + name +'_layer{0}_attention.png'.format(layer),
                     bbox_inches='tight',
                     dpi=100)
-
+        plt.close()
     # Use show when you want to show your attention image while extraction
     # plt.show() 
 
-def save_masked_image(image_list, name, n_layers, n_heads, PATH):
+def save_decoder_image(image_list, name, n_layers, n_heads, PATH, mode='self'):
     fig_saved_dir = PATH
     # input image list has word_num * n_layers * n_heads
     word_num = int(len(image_list) / (n_layers * n_heads))
 
-    img = torch.zeros((n_layers, n_heads, word_num, word_num))
+    img = torch.zeros((n_layers, n_heads, word_num, image_list[-1].size(1)))
 
     for word in range(word_num):
         for layer in range(n_layers):
             for head in range(n_heads):
-                # img_piece size is word + 1 
-                img_piece = image_list[word*(n_layers*n_heads)+layer*(n_heads)+head][0]
-                img[layer, head, word, :word+1] = img_piece
+                if mode == 'self':
+                    # img_piece size is word + 1 
+                    img_piece = image_list[word*(n_layers*n_heads)+layer*(n_heads)+head][0]
+                    img[layer, head, word, :word+1] = img_piece
+                elif mode == 'src':
+                    img_piece = image_list[word*(n_layers*n_heads)+layer*(n_heads)+head][0]
+                    img[layer, head, word] = img_piece
 
     for layer in range(n_layers):
         fig = plt.figure(figsize=(100,100))
@@ -51,22 +55,30 @@ def save_masked_image(image_list, name, n_layers, n_heads, PATH):
         plt.savefig(fig_saved_dir + 'audio' + str(audio_num) + '_' + name +'_layer{0}_attention.png'.format(layer),
                     bbox_inches='tight',
                     dpi=100)
-
+        plt.close()
 
 if __name__ == "__main__":
 
     # encoder's attention extract function
     saved_encoder_self_attn_images = []
     saved_decoder_self_attn_images = []
+    saved_decoder_src_attn_images = []
 
-    def attn_encoder(self, input_tensor, output_tensor):
+    def attn_unmasked_encoder(self, input_tensor, output_tensor):
         cols = output_tensor[1].size(1)
 
         for i in range(cols):
             img = output_tensor[1][0,i,:]
             saved_encoder_self_attn_images.append(img)
 
-    def attn_decoder(self, input_tensor, output_tensor):
+    def attn_unmasked_decoder(self, input_tensor, output_tensor):
+        cols = output_tensor[1].size(1)
+
+        for i in range(cols):
+            img = output_tensor[1][0,i,:]
+            saved_decoder_src_attn_images.append(img)
+
+    def attn_masked_decoder(self, input_tensor, output_tensor):
         cols = output_tensor[1].size(1)
 
         for i in range(cols):
@@ -78,7 +90,7 @@ if __name__ == "__main__":
     WAV_LIST_PATH = TEST_DATA_PATH + "/wav.scp"
 
     file_name_list = []
-    audio_num = 1 # selelct one of the wav in file_name_list
+    audio_num = 5 # selelct one of the wav in file_name_list
 
     with open(WAV_LIST_PATH, "r") as f:
         lines = f.readlines()
@@ -111,15 +123,21 @@ if __name__ == "__main__":
     for name, parameter in net.named_modules():
         for i in range(18): 
             if 'encoder.encoders.'+ str(i) +'.self_attn' == name:
-                parameter.register_forward_hook(attn_encoder)
+                parameter.register_forward_hook(attn_unmasked_encoder)
 
     for name, parameter in net.named_modules():
         for i in range(6): 
             if 'decoder.decoders.'+ str(i) +'.self_attn' == name:
-                parameter.register_forward_hook(attn_decoder)
+                parameter.register_forward_hook(attn_masked_decoder)
+    
+    for name, parameter in net.named_modules():
+        for i in range(6): 
+            if 'decoder.decoders.'+ str(i) +'.src_attn' == name:
+                parameter.register_forward_hook(attn_unmasked_decoder)
 
     # Do forward path calculation for extract image
     out = speech2text(speech)
 
-    # save_unmasked_image(saved_encoder_self_attn_images, 'encoder_self_attn', 18, 8, './exp/feature_images/encoder_self/')
-    save_masked_image(saved_decoder_self_attn_images, 'decoder_self_attn', 6, 8, './exp/feature_images/decoder_self_attn/')
+    # save_encoder_image(saved_encoder_self_attn_images, 'encoder_self_attn', 18, 8, './exp/feature_images/encoder_self_attn/')
+    # save_decoder_image(saved_decoder_self_attn_images, 'decoder_self_attn', 6, 8, './exp/feature_images/decoder_self_attn/', 'self')
+    save_decoder_image(saved_decoder_src_attn_images, 'decoder_src_attn', 6, 8, './exp/feature_images/decoder_src_attn/', 'src')
