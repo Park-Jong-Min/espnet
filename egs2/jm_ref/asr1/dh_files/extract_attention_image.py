ca748fa0884c311ffdaad15a24b5c14c9bc68c4a
Park-Jong-Min <jongmin15@postech.ac.kr>
2021-01-03 00:41:22 +0900

    Move jm_ver0.0 files to jm_ver0.1

---
+++ egs2/jm_ref/asr1/dh_files/extract_attention_image.py
@@ -0,0 +1,139 @@
+import soundfile
+import torch
+import matplotlib.pyplot as plt
+from espnet_model_zoo.downloader import ModelDownloader
+from espnet2.bin.asr_inference import Speech2Text
+
+def save_encoder_image(image_list, audio_num, name, n_layers, n_heads, PATH):
+    fig_saved_dir = PATH
+
+    for layer in range(n_layers):
+        fig = plt.figure(figsize=(100,100))
+        axes = []
+        for head in range(n_heads):
+            img = image_list[layer*n_heads+head]
+            axes.append(fig.add_subplot(1, n_heads, head+1))
+            plt.imshow(img)
+            plt.axis('off')
+
+        print('process {0} layer images....'.format(layer))
+        plt.savefig(fig_saved_dir + 'audio' + str(audio_num) + '_' + name +'_layer{0}_attention.png'.format(layer),
+                    bbox_inches='tight',
+                    dpi=100)
+        plt.close()
+
+def save_decoder_image(image_list, audio_num, name, n_layers, n_heads, PATH, mode='self'):
+    fig_saved_dir = PATH
+    # input image list has word_num * n_layers * n_heads
+    word_num = int(len(image_list) / (n_layers * n_heads))
+
+    img = torch.zeros((n_layers, n_heads, word_num, image_list[-1].size(1)))
+
+    for word in range(word_num):
+        for layer in range(n_layers):
+            for head in range(n_heads):
+                if mode == 'self':
+                    # img_piece size is word + 1 
+                    img_piece = image_list[word*(n_layers*n_heads)+layer*(n_heads)+head][0]
+                    img[layer, head, word, :word+1] = img_piece
+                elif mode == 'src':
+                    img_piece = image_list[word*(n_layers*n_heads)+layer*(n_heads)+head][0]
+                    img[layer, head, word] = img_piece
+
+    for layer in range(n_layers):
+        fig = plt.figure(figsize=(100,100))
+        axes = []
+        for head in range(n_heads):
+            img_save = img[layer][head]
+            axes.append(fig.add_subplot(1, n_heads, head+1))
+            plt.imshow(img_save)
+            plt.axis('off')
+        
+        print(f'process {layer} layer images....')
+        plt.savefig(fig_saved_dir + f'_layer{layer}_attention.png',
+                    bbox_inches='tight',
+                    dpi=100)
+        plt.close()
+
+if __name__ == "__main__":
+
+    exp_dir = '/home/jmpark/home_data_jmpark/espnet/egs2/jm_ref/asr1/exp'
+    # encoder's attention extract function
+    saved_encoder_self_attn_images = []
+    saved_decoder_self_attn_images = []
+    saved_decoder_src_attn_images = []
+
+    def attn_unmasked_encoder(self, input_tensor, output_tensor):
+        cols = output_tensor[1].size(1)
+
+        for i in range(cols):
+            img = output_tensor[1][0,i,:]
+            saved_encoder_self_attn_images.append(img)
+
+    def attn_unmasked_decoder(self, input_tensor, output_tensor):
+        cols = output_tensor[1].size(1)
+
+        for i in range(cols):
+            img = output_tensor[1][0,i,:]
+            saved_decoder_src_attn_images.append(img)
+
+    def attn_masked_decoder(self, input_tensor, output_tensor):
+        cols = output_tensor[1].size(1)
+
+        for i in range(cols):
+            img = output_tensor[1][0,i,:]
+            saved_decoder_self_attn_images.append(img)
+
+    # Set test wav for attention image extraction
+    TEST_DATA_PATH = "./../data/dev_clean"
+    WAV_LIST_PATH = TEST_DATA_PATH + "/wav.scp"
+
+    file_name_list = []
+    audio_num = 2 # selelct one of the wav in file_name_list
+
+    with open(WAV_LIST_PATH, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            num, name = line.split(' ')
+            file_name_list.append(name[:-1])
+
+    speech, rate = soundfile.read(file_name_list[audio_num])
+
+    # Prepare model
+    d = ModelDownloader()
+
+    speech2text = Speech2Text(
+    **d.download_and_unpack('Shinji Watanabe/librispeech_asr_train_asr_transformer_e18_raw_bpe_sp_valid.acc.best'),
+    # Decoding parameters are not included in the model file
+    maxlenratio=0.0,
+    minlenratio=0.0,
+    beam_size=1,
+    ctc_weight=0.4,
+    lm_weight=0.6,
+    penalty=0.0,
+    nbest=1
+    )
+    # Add register hook for in encoder layers.
+    net = speech2text.asr_model
+
+    for name, parameter in net.named_modules():
+        for i in range(18): 
+            if 'encoder.encoders.'+ str(i) +'.self_attn' == name:
+                parameter.register_forward_hook(attn_unmasked_encoder)
+
+    for name, parameter in net.named_modules():
+        for i in range(6): 
+            if 'decoder.decoders.'+ str(i) +'.self_attn' == name:
+                parameter.register_forward_hook(attn_masked_decoder)
+    
+    for name, parameter in net.named_modules():
+        for i in range(6): 
+            if 'decoder.decoders.'+ str(i) +'.src_attn' == name:
+                parameter.register_forward_hook(attn_unmasked_decoder)
+
+    # Do forward path calculation for extract image
+    out = speech2text(speech)
+
+    save_encoder_image(saved_encoder_self_attn_images, audio_num, 'encoder_self_attn', 18, 8, exp_dir + '/feature_images/encoder_self_attn/')
+    # save_decoder_image(saved_decoder_self_attn_images, audio_num, 'decoder_self_attn', 6, 8, exp_dir + '/feature_images/decoder_self_attn/', 'self')
+    # save_decoder_image(saved_decoder_src_attn_images, audio_num, 'decoder_src_attn', 6, 8, exp_dir + '/feature_images/decoder_src_attn/', 'src')
---
+++ egs2/jm_ref/asr1/dh_files/transformer_flops_cal.py
@@ -0,0 +1,33 @@
+import soundfile
+import torch
+import matplotlib.pyplot as plt
+from extract_attention_image import *
+from espnet_model_zoo.downloader import ModelDownloader
+from espnet2.bin.asr_inference import Speech2Text
+
+def matrix_mul_flops(m1_shape, m2_shape, bias=False):
+    assert m1_shape[1] == m2_shape[0], '행렬 연산의 사이즈가 맞지 않습니다.'
+
+    if bias == True:
+        (A, B) = m1_shape
+        (B, C) = m2_shape
+        mat_flops = (2*B + 1) 
+    else:
+        print("Not implemented")
+
+def one_layer_attn_flops_cal(input_shape, d_model, n_heads):
+
+    seqlen, input_dim = input_shape[1], input_shape[2]
+    # linear_q
+    assert input_dim == d_model, '모델 파라미터와 input의 사이즈가 맞지 않습니다.'
+    # FLOPs_linead_q = (d_model * 2 - 1) * (d_model * )
+
+if __name__ == "__main__":
+    # transformer's hyper parameters
+    # linear_q, linear_k, linear_v, linear_out, feed_forward_w_1, feed_forward_w_2, norm1, norm2
+    # 4.815sec speech
+    input_shape = [1, 99, 512]
+
+    one_layer_attn_flops_cal(input_shape, 512, 8)
+
+
