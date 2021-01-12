fd745a5994f8642e87635c250a6af4094b731838
Park-Jong-Min <jongmin15@postech.ac.kr>
2021-01-12 20:42:11 +0900

    Make flops calculator for one encoder layer

--- egs2/jm_ref/asr1/dh_files/transformer_flops_cal.py
+++ egs2/jm_ref/asr1/dh_files/transformer_flops_cal.py
@@ -29,27 +29,44 @@ def self_attn_flops_cal(input_shape, d_model, d_k_q=64, d_v=64, n_heads=8):
     assert input_dim == d_model, '모델 파라미터와 input의 사이즈가 맞지 않습니다.'
 
     # Q, K, V
-    FLOPs_linead_q, FLOPs_linead_k, FLOPs_linead_v = 0, 0, 0
+    FLOPs_linear_q, FLOPs_linear_k, FLOPs_linear_v = 0, 0, 0
     for head in range(n_heads):
-        FLOPs_linead_q += matrix_mul_flops(input_shape, [d_model, d_k_q])
-        FLOPs_linead_k += matrix_mul_flops(input_shape, [d_model, d_k_q])
-        FLOPs_linead_v += matrix_mul_flops(input_shape, [d_model, d_v])
+        FLOPs_linear_q += matrix_mul_flops(input_shape, [d_model, d_k_q])
+        FLOPs_linear_k += matrix_mul_flops(input_shape, [d_model, d_k_q])
+        FLOPs_linear_v += matrix_mul_flops(input_shape, [d_model, d_v])
 
     # Score
     FLOPs_score = 0
     for head in range(n_heads):
-        FLOPs_score += matrix_mul_flops([seqlen, d_k_q], [d_k_q, seqlen], bias=False) + (seq_len * seq_len)
+        FLOPs_score += matrix_mul_flops([seqlen, d_k_q], [d_k_q, seqlen], bias=False) + (seqlen * seqlen)
         # attention
         FLOPs_score += matrix_mul_flops([seqlen, seqlen], [seqlen, d_v], bias=False)
     
     # Concatenate out
     FLOPs_concatenate_out = matrix_mul_flops([seqlen, d_v*n_heads], [d_v*n_heads, d_model]) 
+
+    # Add & Norm
+    FLOPs_add = seqlen * d_model
+    FLOPs_norm = seqlen * d_model * 2
+
+    # PositionwiseFeedForward
+    FLOPs_ffnn = matrix_mul_flops([seqlen, d_model], [d_model, 2048])
+    FLOPs_ffnn += matrix_mul_flops([seqlen, 2048], [2048, d_model])
+
+    # Total FLOPs
+    FLOPs_total = FLOPs_linear_q + FLOPs_linear_k + FLOPs_linear_v + FLOPs_score + FLOPs_concatenate_out + FLOPs_add + FLOPs_norm + FLOPs_ffnn
+
+    return FLOPs_total
+
+
 if __name__ == "__main__":
     # transformer's hyper parameters
     # linear_q, linear_k, linear_v, linear_out, feed_forward_w_1, feed_forward_w_2, norm1, norm2
     # 4.815sec speech
     input_shape = [99, 512]
 
-    self_attn_flops_cal(input_shape, 512, 8)
+    one_layer_FLOPs = self_attn_flops_cal(input_shape, 512, 8)
+
+    print(f"{one_layer_FLOPs/1e9} GFLOPs")
 
 
